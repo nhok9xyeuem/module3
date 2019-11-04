a82fb5800a43a98cee7013e5797a64af04e99e89
nhok9xyeuem <nhuconghaui@gmail.com>
2019-11-04 10:51:16 +0700

    tao cac interface , service , compunent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent
   ],
   imports: [
     BrowserModule
